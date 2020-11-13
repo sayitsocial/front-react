7dd6e41e0586d315c23da380990c8b1be2f4190f
Ovenoboyo <ovenoboyo@gmail.com>
2020-11-13 21:12:22 +0530

    Cleanups And use sass

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
-import './index.css';
-import App from './App';
+import './index.scss';
 import Switch from './router/router'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch/>
-    <App/>
   </BrowserRouter>,
   document.getElementById('root')
 );

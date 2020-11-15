a82821691098e56b6c9baad82ecb400ea7febc22
Ovenoboyo <ovenoboyo@gmail.com>
2020-11-15 17:24:21 +0530

    Somewhat better navbar

--- src/components/generic/Navbar.js
+++ src/components/generic/Navbar.js
@@ -34,40 +34,36 @@ class Navbar extends React.Component {
   render() {
     return (
       <>
-        <BSNavbar expand="lg" className="row main-navbar">
+        <BSNavbar expand='lg' className='row main-navbar'>
           <Col md={3}>
             <BSNavbar.Brand
-              href="#"
-              className="d-flex main-navbar-brand align-items-center"
+              href='#'
+              className='d-flex main-navbar-brand align-items-center'
             >
               <img
                 src={process.env.PUBLIC_URL + "/navbar/sayitsocial.svg"}
-                alt="logo"
-                className="mr-2"
+                alt='logo'
+                className='mr-4'
               ></img>
-              <div className="mb-1"> SayItSocial</div>
+              <div className='mb-1'> SayItSocial</div>
             </BSNavbar.Brand>
           </Col>
           <Col md={6}>
-            <Form inline className="my-2 my-lg-0">
-              <Form.Group className="w-100" controlId="formSearchbar">
-                <Form.Control
-                  className="mr-sm-2 ml-md-5 w-75"
-                  type="search"
-                  placeholder="Search"
-                />
+            <Form inline className='my-2 my-lg-0'>
+              <Form.Group className='w-100' controlId='formSearchbar'>
+                <Form.Control type='search' placeholder='Search' />
               </Form.Group>
             </Form>
           </Col>
-          <Col md={6}>
-            <Nav className="mr-auto">
+          <Col md={3}>
+            <Nav className='float-right nav-icon-group'>
               {this.state.navlinks.map((type, index) => (
                 <Nav.Link href={type.link} key={index}>
                   <img
-                    className="navlink-icon"
+                    className='navlink-icon'
                     src={type.icon}
-                    width="24"
-                    height="24"
+                    width='24'
+                    height='24'
                     alt={type.name}
                   ></img>
                 </Nav.Link>

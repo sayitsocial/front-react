3b547bf56c6412f3ee88e2983097d7cd4568c4cd
Abhishek Choure <abhishekchoure222@gmail.com>
2020-11-15 17:07:17 +0530

    event complete

--- src/components/event/eventEventInfo.js
+++ src/components/event/eventEventInfo.js
@@ -10,7 +10,7 @@ class AttendEventInfo extends React.Component{
                     <div className="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-auto">
                         <h1>Webinar: E-waste Management</h1>
                         <Link to="#">Category: Donations</Link>
-                        <button type="button" className="btn btn-outline-danger">
+                        <button type="button" className="subtype-activity btn btn-outline-danger">
                             Subtype :Activity
                         </button>
                         <br /><br />
@@ -31,14 +31,16 @@ class AttendEventInfo extends React.Component{
                                 <img src={"https://picsum.photos/id/237/100"} alt="" />
                                 </div>
                             </div>
-                            <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-auto">
-                                <h5>Organised by <br />Child in Need Institute</h5>
+                            <div class="event-organizer-info col-xl-6 col-lg-6 col-md-12 col-sm-12 col-auto">
+                                <h5>Organised by <br /><strong>Child in Need Institute</strong></h5>
                                 <p>contact +123 123 1233</p>
-                                <p>
-                                    Value points
-                                    <img src={"https://picsum.photos/id/870/20"} alt="" />
-                                    <b>300</b>
-                                </p>
+                                <span class="flex-points">
+                                    <p>Value points</p>
+                                    <p>
+                                        <img src={"https://picsum.photos/id/870/20"} alt="" />
+                                        <b className="points">300</b>
+                                    </p>
+                                </span>
                             </div>
                         </div>
                     </div>

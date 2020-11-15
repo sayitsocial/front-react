0ed9ca72885d31464c5db0264e27a3bfa30277c5
Abhishek Choure <abhishekchoure222@gmail.com>
2020-11-15 15:02:19 +0530

    event Page

--- src/router/router.js
+++ src/router/router.js
@@ -1,10 +1,13 @@
 import DemoPage from '../Pages/Page1'
+import Event from '../Pages/event/Event'
+
 const { Switch, Route } = require("react-router-dom");
 
 function Routes() {
     return (
         <Switch>
             <Route path="/demo" component={ DemoPage } />
+            <Route path="/event" component={ Event } />
         </Switch>
     )
 }

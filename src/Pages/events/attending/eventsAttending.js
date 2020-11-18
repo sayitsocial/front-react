044a49fc1d175f671c68de04a566c80bcff0cabd
Swanand-Wagh <swanandwagh7@gmail.com>
2020-11-18 11:37:38 +0530

    attending card pushed

--- src/Pages/events/attending/eventsAttending.js
+++ src/Pages/events/attending/eventsAttending.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
+import { Row } from "react-bootstrap";
+import AttendingCard from "../../../components/events/attending/attendingCard";
 import "../../../components/events/attending/attending.scss";
 
 class EventsAttending extends Component {
   render() {
     return (
       <>
-        <h1 className='display-1 text-danger'>Hey Lotus</h1>
+        <h3 className='mx-5 mt-2'>Events You are Attending</h3>
+        <Row className='mx-4'>
+          <AttendingCard />
+        </Row>
       </>
     );
   }

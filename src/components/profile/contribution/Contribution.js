9d863f37afd3146141eecb20cbb7d8efefc79c60
Swanand-Wagh <swanandwagh7@gmail.com>
2021-01-17 12:35:36 +0530

    Pie chart added

--- src/components/profile/contribution/Contribution.js
+++ src/components/profile/contribution/Contribution.js
@@ -2,17 +2,19 @@ import React, { Component } from "react";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import { Col, Row } from "react-bootstrap";
 import "../../../components/profile/profile.scss";
-import PieChart from "../contribution/PieChart";
+import PieGraph from "./PieGraph";
 import Choices from "../contribution/Choices";
 
 class Contribution extends Component {
   render() {
     return (
       <>
-        <div className='contribution'>
-          <Jumbotron className='profile-jumbotron'>
+        <div>
+          <Jumbotron className='profile-jumbotron contribution'>
             <Row>
-              <Col md={6}>{/* <PieChart /> */}</Col>
+              <Col md={6}>
+                <PieGraph />
+              </Col>
               <Col md={6}>
                 <Choices />
               </Col>

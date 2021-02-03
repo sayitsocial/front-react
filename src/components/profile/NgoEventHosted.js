664ffc22cc5e2bd49cdccb30ecd82903a40573f3
Shriya1607 <guptashriya03@gmail.com>
2021-02-03 18:48:31 +0530

    NgoEventHosted completed

--- src/components/profile/NgoEventHosted.js
+++ src/components/profile/NgoEventHosted.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Jumbotron } from "react-bootstrap";
+import { Jumbotron, Row, Col } from "react-bootstrap";
 import AttendingCard from "../events/attending/attendingCard";
 import "../../components/profile/profile.scss";
 
@@ -9,8 +9,17 @@ class NgoEventHosted extends Component {
       <>
         <div className='ngo-event-hosted'>
           <Jumbotron className='profile-jumbotron'>
-            <h3>Events Hosted</h3>
-            <AttendingCard />
+            <h3 style={{ textAlign: "center" }}>Events Hosted</h3>
+            <Row>
+              <AttendingCard />
+              <AttendingCard />
+              <AttendingCard />
+            </Row>
+            <Row>
+              <AttendingCard />
+              <AttendingCard />
+              <AttendingCard />
+            </Row>
           </Jumbotron>
         </div>
       </>

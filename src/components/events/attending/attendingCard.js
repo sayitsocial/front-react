cddae3ed576daa05aafea6cc8a7212dd02c88321
ovenoboyo <ovenoboyo@gmail.com>
2021-03-28 01:03:53 +0530

    Add random api calls

--- src/components/events/attending/attendingCard.js
+++ src/components/events/attending/attendingCard.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Card, Col } from "react-bootstrap";
 
 class AttendingCard extends Component {
-  render() {
+  render () {
     return (
       <>
         <Col md={4} className='mt-4'>
@@ -13,9 +13,9 @@ class AttendingCard extends Component {
             />
             <Card.ImgOverlay className='d-flex justify-content-between '>
               <Card.Title>
-                Donations 23 Aug 2020 <br /> 10pm
+                {this.props.title} 23 Aug 2020 <br /> 10pm
               </Card.Title>
-              <Card.Title>300 Attendees</Card.Title>
+              <Card.Title>{this.props.attendees_count}</Card.Title>
             </Card.ImgOverlay>
           </Card>
         </Col>

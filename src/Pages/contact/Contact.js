4e67119ebca44df78f19a9167e9c184c37e91515
Easwaran <easwaran2806@gmail.com>
2021-03-30 14:57:17 +0530

    Contact us page

--- src/Pages/contact/Contact.js
+++ src/Pages/contact/Contact.js
@@ -1,13 +1,21 @@
 import React, { Component } from "react";
+
 import ContactForm from "../../components/contact/ContactForm";
 import ContactHeader from "../../components/contact/ContactHeader";
+import "../../components/contact/contact.scss";
 
 class Contact extends Component {
   render() {
     return (
       <>
-        <ContactHeader />
-        <ContactForm />
+        <div className='contact-container'>
+          <div className='item1'>
+            <ContactHeader />
+          </div>
+          <div className='item2'>
+            <ContactForm />
+          </div>
+        </div>
       </>
     );
   }
